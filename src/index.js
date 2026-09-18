dc2f4fdad696d8f732d1646463d327ec767a52c0
huey009 <xamxii0012@gmail.com>
2026-09-17 18:48:38 -0600

    app

--- src/index.js
+++ src/index.js
@@ -727,7 +727,9 @@ wss.on('connection', (ws) => {
 // ------------------------------------------------------------
 // MIDDLEWARE
 // ------------------------------------------------------------
-app.use(helmet());
+app.use(helmet({
+  xDownloadOptions: false
+}));
 app.use(cors());
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ extended: true, limit: '100mb' }));
